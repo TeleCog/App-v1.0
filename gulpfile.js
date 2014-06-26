808ab442c0fb591c9e80bdd291c445e66d11ef19
Rishi Shah <rishi@livewiretax.com>
2014-06-26 14:13:06 -0500

    Update build tools (gulp with browserify)

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,80 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+(function () {
+    'use strict';
 
-var paths = {
-  sass: ['./scss/**/*.scss']
-};
+    var gulp = require('gulp'),
+        sass = require('gulp-sass'),
+        rename = require('gulp-rename'),
+        exec = require('exec'),
+        minifyCss = require('gulp-minify-css'),
+        browserify = require('browserify'),
+        source = require('vinyl-source-stream'),
+        path = require('path'),
+        o = require('open'),
+        ripple = require('ripple-emulator'),
 
-gulp.task('default', ['sass']);
+        webPath = function (p) {
+            return path.join('./www/', p);
+        };
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
-});
-
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-});
-
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    gulp.task('refresh', function() {
+        exec('cordova prepare', function () {
+            console.log('Files copied to platform folders');
+        });
     });
-});
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+    // using vinyl-source-stream:
+    gulp.task('scripts', function () {
+        browserify({
+            debug: true
+        })
+            .add('./www/js/app.js')
+            //.transform('debowerify')
+            .bundle()
+            .pipe(source('app.bundle.js'))
+            //.pipe(streamify(uglify()))
+            .pipe(gulp.dest(webPath('js/')));
+    });
+
+    // Compiles the SASS styles
+    gulp.task('sass', function (done) {
+        gulp.src('./scss/ionic.app.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('./www/css/'))
+            .pipe(minifyCss({
+                keepSpecialComments: 0
+            }))
+            .pipe(rename({ extname: '.min.css' }))
+            .pipe(gulp.dest('./www/css/'))
+            .on('end', done);
+    });
+
+    // The default task
+    gulp.task('default', function () {
+        var paths = {
+                sass: ['./scss/**/*.scss'],
+                js: ['./www/js/**/*.js']
+            },
+            options = {
+                keepAlive: false,
+                open: true,
+                port: 4400
+            };
+
+        // Watch the JS directory for changes and re-run scripts task when it changes
+        gulp.watch(paths.js, ['scripts', 'refresh']);
+
+        // Watch the CSS directory for changes and re-run styles task when it changes
+        gulp.watch(paths.sass, ['sass', 'refresh']);
+
+        // Run scripts and styles tasks for the first time
+        gulp.run('scripts');
+        gulp.run('sass');
+
+        // Start the ripple server
+        ripple.emulate.start(options);
+
+        if (options.open) {
+            o('http://localhost:' + options.port + '?enableripple=cordova-3.0.0');
+        }
+    });
+}());
