270dde88625151e7b5a746c2146c49dbe7371b2f
Rishi Shah <rishi@livewiretax.com>
2014-06-26 19:04:51 -0500

    Naming

--- js/controllers/dashboardctrl.js
+++ js/controllers/dashboardctrl.js
@@ -1,6 +1,7 @@
 module.exports = function ($scope, $state, $ionicViewService) {
     'use strict';
 
+    // So that there won't be back button to login page
     $ionicViewService.clearHistory();
 
     console.log($scope, $state);
