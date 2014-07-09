bbbf9db994738af10a7b39f8effbd1c85b4df9b3
Rishi Shah <rishi@livewiretax.com>
2014-07-09 16:55:20 -0500

    Remove Build system as it corrupts Xcode

--- hooks/after_build/normalize_xcode.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'rubygems'
-require 'bundler/setup'
-
-require 'xcodeproj'
-xcproj = Xcodeproj::Project.open("platforms/ios/LiveWireApp.xcodeproj")
-xcproj.recreate_user_schemes
-xcproj.save
