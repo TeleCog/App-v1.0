d6194d0f86336d745f568d1a9c9d5221d82606c0
Rishi Shah <rishi@livewiretax.com>
2014-07-12 01:35:09 -0500

    Filter by institution

--- app/js/controllers/providers.js
+++ app/js/controllers/providers.js
@@ -1,59 +1,79 @@
 angular.module('livewireApp')
 
-    .controller('ProvidersCtrl', function ($scope, $ionicModal, $ionicLoading, ApiService) {
-        'use strict';
+.controller('ProvidersCtrl', function ($scope, $ionicModal, $ionicLoading, ApiService) {
+    'use strict';
 
-        $ionicModal.fromTemplateUrl('/partials/main/_filters.html', {
-            scope: $scope
-        }).then(function (modal) {
-            $scope.modal = modal;
-        });
+    $ionicModal.fromTemplateUrl('/partials/main/_filters.html', {
+        scope: $scope
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
 
-        $ionicLoading.show({
-            template: 'Loading <i class=ion-loading-c></i>'
-        });
+    $ionicLoading.show({
+        template: 'Loading <i class=ion-loading-c></i>'
+    });
 
-        $scope.showFilters = function () {
-            $scope.modal.show();
-        };
+    $scope.showFilters = function () {
+        $scope.modal.show();
+    };
 
-        $scope.closeFilters = function () {
-            $scope.modal.hide();
-        };
+    $scope.closeFilters = function () {
+        $scope.modal.hide();
+    };
 
-        $scope.evalProvider = function (provider) {
-            var specialty_key = '',
-                result = false,
-                count = 0;
+    $scope.evalProvider = function (provider) {
+        var specialty_key = '',
+        institution_key = '',
+        result = false,
+        count = 0;
 
-            // Medical Specialty
-            for (specialty_key in $scope.filterToggles.medical_specialty) {
-                count += 1;
+        // Medical Specialty
+        for (specialty_key in $scope.filterToggles.medical_specialty) {
+            count += 1;
 
-                if ($scope.filterToggles.medical_specialty[specialty_key]
-                        && provider.provider.medical_specialty === specialty_key) {
+            if ($scope.filterToggles.medical_specialty[specialty_key]
+                && provider.provider.medical_specialty === specialty_key) {
                     result = true;
                     break;
                 }
-            }
-            if (count === 0) {
-                result = true; // If customer has not yet filtered
-            }
+        }
+        if (count === 0) {
+            result = true; // If customer has not yet filtered
+        }
 
-            return !!result;
-        };
+        // Institution key is ID
+        for (institution_key in $scope.filterToggles.institutions) {
+            count += 1;
 
-        ApiService.providers.index().then(function () {
-            $scope.providers = ApiService.getApiData().providers.index.providers;
-            $scope.filters = ApiService.getApiData().providers.index.filters;
-            $ionicLoading.hide();
-        }, function () {
-            $ionicLoading.hide();
-            console.log("Error");
-        });
+            if ($scope.filterToggles.institutions[institution_key]) {
+                result = (provider.provider.institution_id === parseInt(institution_key, 10));
 
-        $scope.filterToggles = {
-            medical_specialty: {
+                if (result) {
+                    break;
+                }
             }
-        };
+        }
+
+        return !!result;
+    };
+
+
+
+    ApiService.institutions.index().then(function () {
+        $scope.institutions = ApiService.getApiData().institutions.index;
     });
+
+    ApiService.providers.index().then(function () {
+        $scope.providers = ApiService.getApiData().providers.index.providers;
+        $scope.filters = ApiService.getApiData().providers.index.filters;
+        $ionicLoading.hide();
+    }, function () {
+        $ionicLoading.hide();
+        console.log("Error");
+    });
+
+    $scope.filterToggles = {
+        medical_specialty: {},
+        institutions: {}
+    };
+});
--- app/js/services/api.js
+++ app/js/services/api.js
@@ -1,40 +1,66 @@
 angular.module('livewireApp')
-    .factory('ApiService', function ($http, $ionicViewService, $state, AuthService) {
-        'use strict';
+.factory('ApiService', function ($http, $ionicViewService, $state, AuthService) {
+    'use strict';
 
-        var config = require('../config.json'),
-            apiData = {};
+    var config = require('../config.json'),
+    apiData = {},
 
-        return {
-            getApiData: function () {
-                return apiData;
-            },
+    authFailure = function (status) {
+        if (status === 401) {
+            // Invalidate Access Token (to prevent infinite loop)
+            AuthService.invalidateToken();
 
-            providers: {
-                index: function () {
-                    return $http.get(config.paths.prefix + config.paths.api.providers.index
-                        + '?access_token=' + encodeURIComponent(AuthService.retrieveAccessToken()),
-                        {'headers': {
-                            'Accept': 'application/vnd.livewire+json;version=1'
-                        }})
-                        .success(function (data) {
-                            apiData.providers = apiData.providers || {};
-                            apiData.providers.index = data;
-                        })
-                        .error(function (data, status) {
-                            if (status === 401) {
-                                // Invalidate Access Token (to prevent infinite loop)
-                                AuthService.invalidateToken();
+            // So that there won't be back button to login page
+            $ionicViewService.nextViewOptions({
+                disableAnimate: true,
+                disableBack: true
+            });
+            $state.go('signin');
+        }
 
-                                // So that there won't be back button to login page
-                                $ionicViewService.nextViewOptions({
-                                    disableAnimate: true,
-                                    disableBack: true
-                                });
-                                $state.go('signin');
-                            }
-                        });
-                }
+    };
+
+    return {
+        getApiData: function () {
+            return apiData;
+        },
+
+        providers: {
+            index: function () {
+                return $http.get(config.paths.prefix + config.paths.api.providers.index
+                                 + '?access_token=' + encodeURIComponent(AuthService.retrieveAccessToken()),
+                                 {
+                                     'headers': {
+                                         'Accept': 'application/vnd.livewire+json;version=1'
+                                     }
+                                 })
+                                 .success(function (data) {
+                                     apiData.providers = apiData.providers || {};
+                                     apiData.providers.index = data;
+                                 })
+                                 .error(function (data, status) {
+                                     authFailure(status);
+                                 });
             }
-        };
-    });
+        },
+
+        institutions: {
+            index: function () {
+                return $http.get(config.paths.prefix + config.paths.api.institutions.index
+                                 + '?access_token=' + encodeURIComponent(AuthService.retrieveAccessToken()),
+                                 {
+                                     'headers': {
+                                         'Accept': 'application/vnd.livewire+json;version=1'
+                                     }
+                                 })
+                                 .success(function (data) {
+                                     apiData.institutions = apiData.institutions || {};
+                                     apiData.institutions.index = data;
+                                 })
+                                 .error(function (data, status) {
+                                     authFailure(status);
+                                 });
+            }
+        }
+    };
+});
