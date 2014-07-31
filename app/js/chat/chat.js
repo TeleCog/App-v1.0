c5ebce271ab053148c8d30a8395416af443f0007
Rishi Shah <rishi@livewiretax.com>
2014-07-31 03:03:29 -0500

    Chats

--- app/js/chat/chat.js
+++ app/js/chat/chat.js
@@ -1,7 +1,27 @@
 angular.module('chat', ['firebase'])
 .directive('chatContainer', function () {
     var link = function (scope, elem, attrs) {
-        scope.$watch('providerId', function (newValue, oldValue) {
+        var chatElement = function (message) {
+            var container, label, span, p;
+
+            container = elem[0].getElementsByTagName('ion-content')[0].getElementsByClassName('list')[0];
+
+            // Chat
+            label = document.createElement('label');
+            label.className = 'item-input item-stacked-label';
+
+            span = document.createElement('span');
+            span.className = 'input-label';
+            label.appendChild(span);
+
+            p = document.createElement('p');
+            p.textContent = message;
+            label.appendChild(p);
+
+            container.appendChild(label);
+        };
+
+        scope.$watch('providerId', function (newValue) {
             scope.providerId = newValue;
         });
     };
--- app/js/chat/chatCtrl.js
+++ app/js/chat/chatCtrl.js
@@ -1,36 +1,60 @@
 var config = require('../config.json');
 
-module.exports = function ($scope, $rootScope, $ionicLoading, $firebase, ApiService) {
+module.exports = function ($scope, $rootScope, $ionicScrollDelegate, $firebase, ApiService) {
     'use strict';
 
-    var chatRef, syncChat, customerOnlineRef;
+    var chatRef, syncChat, customerOnlineRef, customerRef, syncCustomer;
 
-    $ionicLoading.show({
-        template: 'Loading <i class=ion-loading-c></i>'
-    });
+    $scope.messages = [];
 
     // Request Customer Information
     ApiService.customers.me().then(function () {
         $rootScope.$on('providersCtrlChatModalShown', function () {
             $scope.user = ApiService.getApiData().customers.me;
-            $ionicLoading.hide();
 
+            // Customer Online Firebase
             customerOnlineRef = new Firebase(config.firebase.chatURL + config.paths.prefix.split(/\.+/g)[1] + "/customer_online_status/" + $scope.user.id);
             customerOnlineRef.set(true);
             customerOnlineRef.onDisconnect().remove();
 
+            // Chat Provider Firebase
             chatRef = new Firebase(config.firebase.chatURL + config.paths.prefix.split(/\.+/g)[1] + '/providers/' + $scope.providerId);
             syncChat = $firebase(chatRef);
-            $scope.messages = syncChat.$asArray();
+
+            // Chat Customer Receive Firebase
+            customerRef = new Firebase(config.firebase.chatURL + config.paths.prefix.split(/\.+/g)[1] + '/customers/' + $scope.user.id);
+            syncCustomer = $firebase(customerRef).$asArray();
+
+            syncCustomer.$watch(function (event) {
+                var indexToRemove;
+
+                if (event.event === 'child_added') {
+                    indexToRemove = (syncCustomer.length === 0) ? 0 : syncCustomer.length - 1;
+
+                    $scope.messages.push(syncCustomer[indexToRemove]);
+
+                    // Make the chat window scroll to the bottom 
+                    $ionicScrollDelegate.scrollBottom();
+
+                    syncCustomer.$remove(indexToRemove);
+                }
+            });
 
             $scope.addMessage = function (message) {
-                console.log(message);
-                $scope.messages.$add({
+                var packet = {
                     id: $scope.user.id,
                     name: $scope.user.first_name,
                     message: message,
                     time: Date.now(),
-                    image: ''
+                    image: 'https://www.livewiremedical.com/assets/default_customer.png'
+                };
+
+                syncChat.$push(packet).then(function () {
+                    packet.name = 'Me';
+                    $scope.messages.push(packet);
+
+                    // Make the chat window scroll to the bottom 
+                    $ionicScrollDelegate.scrollBottom();
                 });
             };
         });
